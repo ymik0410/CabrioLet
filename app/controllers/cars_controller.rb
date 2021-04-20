f5e82de4cde1c1550d2fcae932d44084f009aa30
Yulia Mikhaylova ymik0410@gmail.com <ymik0410@gmail.com>
2021-04-20 15:27:13 +0200

    new and create actions

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,4 +6,25 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
   end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      raise
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:title, :model, :year, :city, :description, :price, :photo)
+  end
 end
