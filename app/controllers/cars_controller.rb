86ba386463fa18da862c56863dcba898d5193d46
Santa Cadusch <santacadusch@icloud.com>
2021-04-21 18:27:01 +0200

    routes

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,10 +1,15 @@
 class CarsController < ApplicationController
+  before_action :set_car, only: [:show, :edit, :update, :destroy]
+
   def index
     @cars = Car.all
   end
 
+  def my_cars
+    @cars = Car.where(user_id: current_user)
+  end
+
   def show
-    @car = Car.find(params[:id])
   end
 
   def new
@@ -22,9 +27,27 @@ class CarsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @car.update(car_params)
+    redirect_to car_path(@car)
+  end
+
+  def destroy
+    @car.destroy
+    redirect_to cars_path
+  end
+
   private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
 
   def car_params
     params.require(:car).permit(:title, :model, :year, :city, :description, :price, :photo)
   end
+
 end
