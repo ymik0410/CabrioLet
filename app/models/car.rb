9b730753803f2db714e5cc1413a3b35bb9cfe1db
JuSchlup <jukobe@gmail.com>
2021-04-22 18:21:41 +0200

    reView

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,7 +4,7 @@ class Car < ApplicationRecord
   has_many :bookings, dependent: :destroy
   validates :title, :model, :year, :city, :description, :price, presence: true
   geocoded_by :city
-  has_many :rewiews, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   def unavailable_dates
     bookings.pluck(:start_date, :end_date).map do |range|
