df77610ebc3266f2711d870dbe0c3c56c67bbc4b
Santa Cadusch <santacadusch@icloud.com>
2021-04-22 15:14:31 +0200

    added dependant

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,7 @@
 class Car < ApplicationRecord
   has_one_attached :picture
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :title, :model, :year, :city, :description, :price, presence: true
   geocoded_by :city
   after_validation :geocode, if: :will_save_change_to_city?
