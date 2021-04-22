3424f812853439fa9bc5ec7d4adc8cdf83d37b41
Yulia Mikhaylova ymik0410@gmail.com <ymik0410@gmail.com>
2021-04-22 13:56:59 +0200

    Search

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,4 +4,11 @@ class Car < ApplicationRecord
   validates :title, :model, :year, :city, :description, :price, presence: true
   geocoded_by :city
   after_validation :geocode, if: :will_save_change_to_city?
+
+  include PgSearch::Model
+  pg_search_scope :search,
+    against: [:title, :model, :description, :year],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
